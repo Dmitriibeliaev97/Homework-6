2570812516622b5c984ed333040fd52b6134bd3a
Dmitrii Beliaev <dmitriymay97@gmail.com>
2023-02-06 17:00:58 +0300

    Выполнил задание 5

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,9 @@ public class Main {
         task2();
         task3();
         task4();
+        task5();
+        task6();
+        task7();
     }
 
     public static void task1 () {
@@ -30,5 +33,17 @@ public class Main {
         for (int i = 10; i >= -10; i--) {
             System.out.println(i);
         }
+    }
+    public static void task5 () {
+        System.out.println("Задача 5");
+        for (int i = 1904; i <= 2096; i = i + 4) {
+            System.out.println(i + " год является високосным");
+        }
+    }
+    public static void task6 () {
+        System.out.println("Задача 6");
+    }
+    public static void task7 () {
+        System.out.println("Задача 7");
     }
 }
