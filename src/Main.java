33bfffa8e2ddfe184c820c93cc79a7a002cd71a1
Dmitrii Beliaev <dmitriymay97@gmail.com>
2023-02-06 16:42:02 +0300

    Выполнил задание 2

--- src/Main.java
+++ src/Main.java
@@ -15,7 +15,9 @@ public class Main {
 
     public static void task2 () {
         System.out.println("Задача 2");
-         // Пишем код для задачи 2
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(i);
+        }
     }
     public static void task3 () {
         System.out.println("Задача 3");
